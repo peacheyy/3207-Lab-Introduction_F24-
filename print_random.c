7688f9a41095c865b5a564b419a7fb64367dbb83
Evan Peachey <evanpeachey@gmail.com>
2024-08-30 19:21:51 -0400

    updated code for resubmit

--- print_random.c
+++ print_random.c
@@ -1,29 +1,19 @@
-//Given Project code
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "random.h"
 
-void rand_string(char *str, size_t size) {
-    if (size) {
-        for (size_t n = 0; n < size; n++) {
-            str[n] = randchar();
-        }
-        str[size] = '\0';
-    }
-}
-
-char* rand_string_alloc(size_t size)
+int main()
 {
-    char *s = malloc(size + 1);
-    if (s) {
-        rand_string(s, size);
-    }
-    return s;
-}
+	int a;
 
-int main(void) {
-    srand((unsigned int)time(NULL));
-    char *random_word = rand_string_alloc(7);
-    printf("%s\n", random_word);
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+
+	return(0);
 }
